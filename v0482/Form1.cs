d72a875bfbbee76f5210ad0654ab91029f5575d5
reiji-dat <reiji.dat@gmail.com>
2022-05-12 15:44:30 +0900

    ボタンを消す

--- v0482/Form1.cs
+++ v0482/Form1.cs
@@ -19,8 +19,9 @@ namespace v0482
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int a = 0;
+            int a = 1;
             MessageBox.Show("" + (10 /a));
+            mybutton.Visible = false;
         }
     }
 }
